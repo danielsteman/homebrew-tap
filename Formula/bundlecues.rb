c0dc585eba70fd8365d6708d0068e69675b8a681
Daniel Steman <daniel-steman@live.nl>
2025-04-04 11:37:49 +0200

    add bundlecues

---
+++ Formula/bundlecues.rb
@@ -0,0 +1,16 @@
+class Bundlecues < Formula
+  desc "Validate Databricks asset bundles using CUE"
+  homepage "https://github.com/YOUR_USERNAME/bundlecues"
+  url "https://github.com/danielsteman/bundlecues/releases/download/v0.1.0/bundlecues_darwin_arm64.tar.gz"
+  sha256 "bc752f7a266c5f8d7d77245f04f27feaa3e644d3e124b0e8851c911161e13be0"
+  license "MIT"
+
+  def install
+    bin.install "bundlecues"
+  end
+
+  test do
+    system "#{bin}/bundlecues", "--help"
+  end
+end
+
