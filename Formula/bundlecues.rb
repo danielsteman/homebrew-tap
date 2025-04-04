6d72ea882b4f1b7f4b36ebe508786f3e9c88d92b
Daniel Steman <daniel-steman@live.nl>
2025-04-04 15:22:15 +0200

    add checksum for amd bin

--- Formula/bundlecues.rb
+++ Formula/bundlecues.rb
@@ -10,7 +10,7 @@ class Bundlecues < Formula
       sha256 "091c254d5a215880a5944c0c8b371b87e058ca3748d966fe8f596c898dfb65a0"
     elsif Hardware::CPU.intel?
       url "https://github.com/danielsteman/bundle-cues/releases/download/v0.1.0/bundlecues_darwin_amd64.tar.gz"
-      sha256 "REPLACE_WITH_AMD64_SHA"
+      sha256 "fb5a979b5f32314784e7186b0529c4cf280f50852b75f1fc9f2ce4d8c91cdc41"
     end
   end
 
