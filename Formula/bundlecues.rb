eb8e211d1fbd4f3f7e1a8caa5025918c51fe5ed7
Daniel Steman <daniel-steman@live.nl>
2025-04-04 11:46:29 +0200

    update repo url

--- Formula/bundlecues.rb
+++ Formula/bundlecues.rb
@@ -1,7 +1,7 @@
 class Bundlecues < Formula
   desc "Validate Databricks asset bundles using CUE"
-  homepage "https://github.com/danielsteman/bundlecues"
-  url "https://github.com/danielsteman/bundlecues/releases/download/v0.1.0/bundlecues_darwin_arm64.tar.gz"
+  homepage "https://github.com/danielsteman/bundle-cues"
+  url "https://github.com/danielsteman/bundle-cues/releases/download/v0.1.0/bundlecues_darwin_arm64.tar.gz"
   sha256 "091c254d5a215880a5944c0c8b371b87e058ca3748d966fe8f596c898dfb65a0"
   license "MIT"
 
