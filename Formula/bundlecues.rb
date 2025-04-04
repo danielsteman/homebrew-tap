ff069000247f590545e6ac1fddb0e7895b1b6f71
Daniel Steman <daniel-steman@live.nl>
2025-04-04 11:52:06 +0200

    update formula

--- Formula/bundlecues.rb
+++ Formula/bundlecues.rb
@@ -1,9 +1,18 @@
 class Bundlecues < Formula
   desc "Validate Databricks asset bundles using CUE"
   homepage "https://github.com/danielsteman/bundle-cues"
-  url "https://github.com/danielsteman/bundle-cues/releases/download/v0.1.0/bundlecues_darwin_arm64.tar.gz"
-  sha256 "091c254d5a215880a5944c0c8b371b87e058ca3748d966fe8f596c898dfb65a0"
+  version "0.1.0"
   license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/danielsteman/bundle-cues/releases/download/v0.1.0/bundlecues_darwin_arm64.tar.gz"
+      sha256 "091c254d5a215880a5944c0c8b371b87e058ca3748d966fe8f596c898dfb65a0"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/danielsteman/bundle-cues/releases/download/v0.1.0/bundlecues_darwin_amd64.tar.gz"
+      sha256 "REPLACE_WITH_AMD64_SHA"
+    end
+  end
 
   def install
     bin.install "bundlecues"
